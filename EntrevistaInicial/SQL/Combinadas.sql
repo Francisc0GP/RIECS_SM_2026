a8d6b9fd603ce6c93b09bed0ec18b0e6e13cfb1f
Francisc0GP <159593831+Francisc0GP@users.noreply.github.com>
2026-01-27 17:13:46 -0600

    Entrevista_Inicial/ Extrac_script
    Implementación del primer script para extraer información desde SQL Server, realizar un preprocesamiento básico y subir el resultado (CSV) a un servidor FTP.

---
+++ EntrevistaInicial/SQL/Combinadas.sql
@@ -0,0 +1,4 @@
+SELECT EI.FolioId, EISC.SustanciaId AS EISC_SustanciaId
+FROM THSDSistemaCIJ.adm.ECEEntrevistaInicial EI
+LEFT JOIN THSDSistemaCIJ.adm.ECEEntrevistaInicialSustanciasCombinadas EISC ON EI.FolioId = EISC.FolioId
+WHERE YEAR(EI.FechaRegistro) = 2025
